50ee79e887b017dbb290cbb055b95427209c46c8
Percival <emperorsheevpalpatine0@gmail.com>
2024-04-24 14:17:02 +0800

    Refactor: home.view.php Implemented method "loadView()"

--- helpers.php
+++ helpers.php
@@ -7,4 +7,14 @@ function loadPartial($name) {
     require basePath("views/partials/{$name}.php");
 }
 
+function loadView($name) {
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View {$viewPath} doesn't exist!";
+    }
+}
+
+
 ?>
--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
 //error inspect
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 require '../helpers.php';
-require basePath('views/home.view.php');
+require loadView('home')
 ?>
