45c329123fc83ed64f24eff6519f261267cf8859
Percival <emperorsheevpalpatine0@gmail.com>
2024-04-26 03:57:54 +0800

    config: Added database configuration program

--- controllers/home.php
+++ controllers/home.php
@@ -1,2 +1,8 @@
 <?php
-loadView('home');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+$listings = $db->query('SELECT * FROM listing LIMIT 6')->fetchAll();
+inspect($listings);
+
+
+//loadView('home');
