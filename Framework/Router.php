0b8e21757e2864d3b6658d95fc292cae1334718a
Percival <emperorsheevpalpatine0@gmail.com>
2024-04-29 17:39:34 +0800

    chore: structural changes

--- Framework/Router.php
+++ Framework/Router.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Framework;
 //error inspect
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '09f41c655e07edbaa0069c90d7b3b1b41bbc3618',
+        'name' => 'myvendor/mypackage',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myvendor/mypackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '09f41c655e07edbaa0069c90d7b3b1b41bbc3618',
+            'dev_requirement' => false,
+        ),
+    ),
+);
